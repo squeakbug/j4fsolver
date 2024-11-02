25020c27a10dd80fbeb1e3717d33e1372394bfd0
squeakbug <squeakbug73@outlook.com>
2024-11-02 22:52:52 +0300

    WIP
    Signed-off-by: squeakbug <squeakbug73@outlook.com>

--- src/LibSolver/Proposition/Forward.hs
+++ src/LibSolver/Proposition/Forward.hs
@@ -1,42 +1,110 @@
 {-# LANGUAGE DatatypeContexts #-}
 
 module LibSolver.Proposition.Forward
-    ( proofFact
+    ( Fact(..)
+    , bfsFromGoal
+    , bfsFromPredicate
     ) where
+import LibSolver.Search.Bfs (bfsFromPredicate)
 
 class Fact a where
     isTautology :: a -> Bool
 
+-- TODO: restrict only for proofed facts
 class (Fact a) => SearchRule a where
-    implies :: a -> Maybe a
-
-newtype (SearchRule a) => RuleStore a = RuleStore [a]
-newtype (Fact a) => FactStore a = FactStore [a]
+    implies :: [f] -> r -> Maybe a
 
 data (Fact f) => ProofResult f = ProofResult
-    { prPath      :: [f]
-    , pfIsProofed :: Bool
+    { prPath         :: [f]
+    , pfIsProofed    :: Bool
+    , pfFinishedFact :: f
     }
 
 data (Fact f, SearchRule r) => ProofState f r = ProofState
-    { psFactStore    :: FactStore f -- Исходный граф
-    , psRuleStore    :: RuleStore r -- Предикат: вершина является целевой
-    , psQueue        :: [r]         -- Очередь
-    , psProofedFacts :: [f]         -- Доказанные факты
-    , psProofedRules :: [r]         -- Доказанные правила
-    , psPath         :: [f]         -- Путь от данных к цели
-    , psIsFinished   :: Bool        -- Завершен вывод?
+    { psFactStore     :: [f]       -- Исходная база фактов
+    , psRuleStore     :: [r]       -- Исходная база правил
+    , psQueue         :: [r]       -- Очередь недоказанных правил
+    , psProofedFacts  :: [f]       -- Список доказанных фактов
+    , psProofedRules  :: [r]       -- Список доказанных правил
+    , psIsFactToProof :: f -> Bool -- Цель доказательства
+    , psPath          :: [f]       -- Путь по правилам
+    , psIsFinished    :: Bool      -- Завершен вывод?
+    }
+
+fromPredicate :: (Fact f, SearchRule r) => [f] -> [f] -> [r] -> (f -> Bool) -> ProofState f r
+fromPredicate fs npfs rs p = ProofState
+    { psFactStore = fs
+    , psRuleStore = rs
+    , psQueue = rs
+    , psProofedFacts = filter (`notElem` npfs) fs
+    , psProofedRules = []
+    , psIsFactToProof = p
+    , psPath = []
+    , psIsFinished = False
+    }
+
+fromGoal :: (Eq f, Fact f, SearchRule r) => [f] -> [f] -> [r] -> f -> ProofState f r
+fromGoal fs npfs rs goal = fromPredicate fs npfs rs (== goal)
+
+fromProofState :: (Fact f, SearchRule r) => ProofState f r -> ProofResult f
+fromProofState ProofState
+    { psPath=path@(p:_)
+    , psQueue=[]
+    } = ProofResult
+    { prPath=path
+    , pfIsProofed=True
+    , pfFinishedFact=p
+    }
+
+fromProofState ProofState
+    { psPath=path@(p:_)
+    } = ProofResult
+    { prPath=path
+    , pfIsProofed=True
+    , pfFinishedFact=p
     }
 
 -----------------------------------------------------------------------------
 
+proofStepHelper :: (Fact f, SearchRule r) => [r] -> [f] -> [(r, f)] -> [(r, f)]
+proofStepHelper [] _ result = result
+proofStepHelper (r:rs) fs result =
+    case implies fs r of
+        Just f -> proofStepHelper rs (f:fs) result
+        Nothing -> proofStepHelper rs fs result
+
 -- Очередь пуста, а вершина не найдена -> заканчиваем поиск
 proofStep :: (Fact f, SearchRule r) => ProofState f r -> ProofState f r
 proofStep ps@ProofState
-    { psFactStore=FactStore []
+    { psFactStore=[]
     } = ps
     { psIsFinished=True
     }
+
+-- Очередь не пуста. Просматриваем список правил в поисках правила, которое можем доказать
+-- Если ничего не может доказать, то заканчиваем доказательство
+proofStep ps@ProofState
+    { psFactStore=fs
+    , psRuleStore=rs
+    , psQueue=q
+    , psProofedFacts=pf
+    , psProofedRules=pr
+    , psIsFactToProof=isFactToProof
+    , psPath=path
+    , psIsFinished=isFinished
+    } = ps
+    { psQueue=q'
+    , psProofedFacts=pf'
+    , psProofedRules=pr'
+    , psIsFinished=psIsFinished'
+    } where (newProofedRules, newProofedFacts) = unzip $ proofStepHelper rs fs []
+            (psIsFinished', isProofed') = case newProofedFacts of
+                [] -> True
+                _ -> False
+            pf' = pf ++ newProofedFacts
+            pr' = pr ++ newProofedRules
+            q' = filter (`notElem` pr) rs
+            psIsFinished'' = foldMap isFactToProof newProofedFacts
 
 proofHelper :: (Fact f, SearchRule r) => ProofState f r -> ProofState f r
 proofHelper initState =
@@ -45,6 +113,9 @@ proofHelper initState =
 
 -----------------------------------------------------------------------------
 
-proofFact :: (Fact f, SearchRule r) => FactStore f -> RuleStore r -> f -> ProofResult f
-proofFact factStore ruleStore fact = ProofResult { prPath = [], pfIsProofed = True}
+bfsFromGoal :: (Fact f, SearchRule r) => [f] -> [r] -> (f -> Bool) -> ProofResult f
+bfsFromGoal fs rs p = fromProofState $ proofHelper $ fromPredicate fs fs rs p
+
+proofFact :: (Fact f, SearchRule r) => [f] -> [r] -> f -> ProofResult f
+proofFact fs rs goal = fromProofState $ proofHelper $ fromGoal fs fs rs goal
 
