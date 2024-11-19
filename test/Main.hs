55711b0b8eeb4ed9a58352c395a7abd75a217f88
squeakbug <squeakbug73@outlook.com>
2024-11-19 19:52:33 +0300

    WIP
    Signed-off-by: squeakbug <squeakbug73@outlook.com>

--- test/Main.hs
+++ test/Main.hs
@@ -1,30 +1,36 @@
 module Main (main) where
 
-import Data.Maybe
-
 import Test.Hspec
 
 import LibSolver.Search
-import LibSolver.Search.Core
+import LibSolver.Search.Uninformed
 
 data IntProblem s a = IntProblem
     { pStart   :: Int
-    , pGoal    :: Int
+    , pRadius    :: Int
     }
 
 instance Problem IntProblem Int Int where
     initial (IntProblem start _) = start
-    goal (IntProblem _ goal') = goal'
+    goalTest (IntProblem _ rad) coord = abs coord >= rad
     successor (IntProblem _ _) cur = [(-1, cur - 1), (1, cur + 1)]
 
 main :: IO ()
 main = hspec $ do
-  describe "graphSearch" $ do
+    describe "breadthFirstGraphSearch" $ do
 
-    it "finds the goal in simple case" $ do
-        let problem :: IntProblem Int Int = IntProblem { pStart = 0, pGoal = 5 }
-        let initialQueue = []
-        let result = graphSearch initialQueue problem
-        let goal' :: Maybe Int = fmap state result
+        it "finds the goal in simple case" $ do
+            let problem :: IntProblem Int Int = IntProblem { pStart = 0, pRadius = 5 }
+            let result = breadthFirstGraphSearch problem
+            let goal' :: Maybe Int = fmap (abs . state) result
 
-        goal' `shouldBe` Just 5
+            goal' `shouldBe` Just 5
+
+    describe "depthFirstGraphSearch" $ do
+
+        it "finds the goal in simple case" $ do
+            let problem :: IntProblem Int Int = IntProblem { pStart = 0, pRadius = 5 }
+            let result = depthFirstGraphSearch problem
+            let goal' :: Maybe Int = fmap (abs . state) result
+
+            goal' `shouldBe` Just 5
